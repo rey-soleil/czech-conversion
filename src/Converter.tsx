178b89aba834670e002221381b9a188df88a2861
rey-soleil <68834278+rey-soleil@users.noreply.github.com>
2022-10-20 15:34:29 -0700

    simple conversion

--- src/Converter.tsx
+++ src/Converter.tsx
@@ -1,38 +1,61 @@
 import React, { useState } from "react";
-import { useQuery } from "react-query";
 
 const cnbURL = 'https://www.cnb.cz/en/financial-markets/foreign-exchange-market/central-bank-exchange-rate-fixing/central-bank-exchange-rate-fixing/daily.txt';
 
+// Country|Currency|Amount|Code|Rate
+// Australia|dollar|1|AUD|15.771
+// Brazil|real|1|BRL|4.770
+// Bulgaria|lev|1|BGN|12.538
+
+const exampleExchangeData = [
+    {
+        country: 'Australia',
+        currency: 'dollar',
+        amount: 1,
+        code: 'AUD',
+        rate: 15.771,
+    },
+    {
+        country: 'Brazil',
+        currency: 'real',
+        amount: 1,
+        code: 'BRL',
+        rate: 4.770,
+    },
+    {
+        country: 'Bulgaria',
+        currency: 'lev',
+        amount: 1,
+        code: 'BGN',
+        rate: 12.538,
+    },
+]
+
 export function Converter(){
     const [czechValue, setCzechValue] = useState(0);
+    const [selectedExchangeData, setSelectedExchangeData] = useState(exampleExchangeData[0]);
+    const [convertedValue, setConvertedValue] = useState(0);
 
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setCzechValue(Number(event.target.value));
     }
 
-    const { status, data, error, isFetching } = useQuery(
-        ['data'],
-        async () => {
-          const data = await (
-            await fetch(cnbURL)
-          ).json()
-          return data
-        }
-    )
-
-    console.log(data);
+    const convert = () => {
+        setConvertedValue(czechValue * selectedExchangeData['rate']);
+    }
     
     return (
         <>
-            <form>
+            <form onSubmit={convert}>
                 <input type='text' value={czechValue} onChange={handleChange}/>
                 <select name='currency'>
-                    <option>Pounds</option>
-                    <option>Euros</option>
-                    <option>Yen</option>
+                    {exampleExchangeData.map(value => (
+                        <option key={value.code}>{value.code} ({value.country} {value.currency})</option>
+                    ))}
                 </select>
                 <input type='submit' value='convert'/>
             </form>
+            {convertedValue && <div>{convertedValue}</div>}
         </>
     );
 }
